0fd931abfa2d107367bbcd96fa0c4529164248c2
Natalia Lizurej <74413186+NataliaLizurej@users.noreply.github.com>
2021-05-25 22:08:15 +0200

    Add files via upload

---
+++ kcppZadania/ZadStruct.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct Users {
+    string login;
+    string password;
+};
+
+int main() {
+    Users A = {"A", "aa"};
+    Users B = {"B", "bb"};
+
+    Users users[3] = {A, B};
+    for (int i=0; i<2; i++) {
+        cout << users[i].login << endl;
+        cout << users[i].password << endl;
+    }
+
+    cout << users[0].login << endl;
+    users[0].login = "a";
+    cout << users[0].login << endl;
+    
+
+
+}
